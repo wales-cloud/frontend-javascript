f20c72b967cc73c10153411e38cde385967c3f67
Adewale Salami <wales.abbe@gmail.com>
2025-05-24 17:51:04 -0600

    Task 2: Extend Teacher interface to define Directors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,29 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Define the base interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+// Extend Teacher to create Directors
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Test case
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
